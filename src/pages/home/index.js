78ef6f0b78b7e1e51c0412a03fef59ba43221ea6
Kelvin Novais <kelvin.novais@protonmail.com>
2022-03-23 09:14:07 -0300

    All home components added,but still missing reponsiveness

--- src/pages/home/index.js
+++ src/pages/home/index.js
@@ -1,18 +1,46 @@
-import react from 'react';
-import { Text, View, Image } from 'react-native';
-import Style from './styles';
+import React from "react";
+import { Text, View, Image, SafeAreaView, TextInput, Pressable } from "react-native";
+import Style from "./styles";
 
 function Home() {
-    return(
-        <View style={Style.bgcolor}>
-            <View style={Style.border}>
-            <View style={Style.borderToBorder}>
-            <Image source={require('../../assets/images/Logo.png')} style={Style.img} />
-            <Text>Testing...</Text>
+  const [username, onChangeUsername] = React.useState("");
+  const [passwd, onChangePasswd] = React.useState("");
+  return(
+    <View style={Style.bgcolor}>
+      <View style={Style.border}>
+          <View style={Style.borderToBorder}>
+            <View style={Style.logo}>
+              <Image source={require("../../assets/images/Logo.png")} style={Style.img} />
             </View>
+            <View style={Style.inputs}>
+              <SafeAreaView>
+                <TextInput
+                  style={Style.input}
+                  onChangeText={onChangeUsername}
+                  value={username}
+                  placeholder="Login"
+                  placeholderTextColor="#474747"
+                />
+                <TextInput
+                  style={Style.input}
+                  onChangeText={onChangePasswd}
+                  value={passwd}
+                  placeholder="Senha"
+                  placeholderTextColor="#474747"
+                  secureTextEntry={true}
+                />
+              </SafeAreaView>
+              <View style={Style.buttons}>
+                <Pressable style={Style.button}>
+                  <Text style={Style.button_text}>Entrar</Text>
+                </Pressable> 
+                <Text style={Style.forgot}>Esqueci a senha</Text>
+              </View>
             </View>
-        </View>
-    );
+          </View>
+      </View>
+    </View>
+  );
 }
 
 export default Home;
--- src/pages/home/styles.js
+++ src/pages/home/styles.js
@@ -1,37 +1,83 @@
 import { StyleSheet, Dimensions } from "react-native";
 
-const winWidth = Dimensions.get('window').width;
-const winHeight = Dimensions.get('window').height;
+const winWidth = Dimensions.get("window").width;
+const winHeight = Dimensions.get("window").height;
 
 const Style = StyleSheet.create({
   bgcolor: {
     backgroundColor: "#4C96A5",
     width: winWidth,
     height: winHeight,
-    padding: 5,
+    padding: 5
   },
   border: {
     borderColor: "#474747",
     borderStyle: "solid",
-    borderWidth: 2,
-
+    borderWidth: 0.3,
     width: winWidth - 10,
     height: winHeight - 10,
-  },
-  borderToBorder: {
-    borderColor: "#474747",
-    borderStyle: "solid",
-    borderWidth: 2,
-
-    width: winWidth - 20,
-    height: winHeight - 20,
     margin: "auto",
     alignItems: "center",
     justifyContent: "center"
   },
+  borderToBorder: {
+    borderColor: "#474747",
+    borderStyle: "solid",
+    borderWidth: 0.3,
+    width: winWidth - 20,
+    height: winHeight - 20,
+    justifyContent: "space-around"
+  },
+  logo: {
+    flex: 1,
+    justifyContent: "flex-end",
+    alignItems: "flex-start",
+    marginHorizontal: 35
+  },
+  inputs: {
+    flex: 3,
+    justifyContent: "flex-start",
+    alignItems: "flex-start",
+    paddingTop: 40,
+    marginHorizontal: 45,
+  },
   img: {
     width: 135,
     height: 64
+  },
+  input: {
+    height: 30,
+    minWidth: 250,
+    margin: 12,
+    color: "#474747",
+    borderColor: "#474747",
+    fontSize: 16,
+    borderBottomWidth: 0.3,
+    paddingTop: 10,
+  },
+  buttons: {
+    paddingTop: 15,
+    justifyContent: "flex-end",
+    alignItems: "center",
+  },
+  button: {
+    //alignSelf: "flex-end",
+    alignItems: "center",
+    justifyContent: "center",
+    paddingVertical: 6,
+    paddingHorizontal: 28,
+    borderColor: "#474747",
+    borderWidth: 0.3,
+  },
+  button_text: {
+    color: "#474747",
+    fontSize: 16
+  },
+  forgot: {
+    fontSize: 14,
+    color: "#474747",
+    textDecorationLine: "underline",
+    paddingTop: 7
   }
 });
 
