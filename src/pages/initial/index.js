3f8962042bf7ed698f8948b38fb1236e5ff3b417
DiogolimalucasDev <diogolimalucas.dev@gmail.com>
2022-05-09 16:46:19 -0300

    get funcionarios + map

--- src/pages/initial/index.js
+++ src/pages/initial/index.js
@@ -5,18 +5,79 @@ import Footer from "../../components/footer/index";
 import Profile from "../../components/profile";
 import { View } from "react-native";
 import Styles from "./styles";
+import { useState } from "react";
 
 export default function Inicio() {
+  const [funcionario, setfuncionario] = useState([]);
+  
+  
+  
+  async function buscarFuncionario() {
+    let response = await fetch('http://192.168.100.14:3000/funcionarios', {
+      method: 'GET',
+
+    });
+    let json = await response.json()
+    console.log(json)
+
+    json.map((object) => {
+      // let phrase = `nome: ${object.nome} Cargo ${object.cargo}\n`;
+      // console.log(phrase)
+      const funcionario = {
+        nome: object.nome,
+        cargo: object.cargo
+      }
+      setfuncionario(funcionario)
+
+      console.log(`nome: ${funcionario.nome} Cargo ${funcionario.cargo}`)
+
+
+    });
+
+  }
+
+
+
+  buscarFuncionario()
+
+
+
+// console.log("esse é o set" + setfuncionario)
+
+  // console.log(buscarFuncionario())
+ 
+  // }
+  // const Item = ({}) => (
+    
+  //   <View>
+  //     <Profile
+
+  //       imageSource={require("../../assets/images/user.png")}
+  //       name="Kelvin Novais"
+  //       job="Fullstack Developer"
+  //     />
+  //   </View>
+  // );
+
+  // const renderItem = ({ item }) => {
+  //   return (<Item line={item.line} name={item.name} prefix={item.prefix}
+
+  //   />)
+  // };
+
+
+
   return (
     <>
       <Header />
       <Content>
         <View style={Styles.container}>
-          <Profile
-            imageSource={require("../../assets/images/user.png")}
-            name="Kelvin Novais"
-            job="Fullstack Developer"
-          />
+
+           <Profile
+              imageSource = { require("../../assets/images/user.png") }
+            name = "Guilherme Severiano"
+            job = "Front-End Developer"
+            />
           <Profile
             imageSource={require("../../assets/images/user.png")}
             name="Guilherme Severiano"
@@ -50,5 +111,5 @@ export default function Inicio() {
       </Content>
       <Footer />
     </>
-  );
+  )
 }
